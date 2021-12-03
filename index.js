672999108f65c0504b76dc55ac7658b815d835e3
dotnotation <dotinmotionproductions@gmail.com>
2021-12-03 13:05:05 -0500

    commit tree 0eb10b41d0831bea12dde48dcba16814be0aa78f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (array) => [array[0], array[1]];
+
+const returnLastTwoDrivers = (array) => [array[array.length - 2], array[array.length - 1]];
+
+const selectingDrivers =  [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer){
+    return (fare) => fare * integer;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driverFunction){
+    return driverFunction(drivers);
+}
